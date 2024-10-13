c338612bae2a4a37ed14e07fe6d38c1717d4dc24
rikutan <163800982+rikutan@users.noreply.github.com>
2024-10-13 21:45:12 +0900

    test

--- app/src/main/java/com/example/udemypractice1/MainActivity.kt
+++ app/src/main/java/com/example/udemypractice1/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             UdemyPractice1Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Ariga Ruriha",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
