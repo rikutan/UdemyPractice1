70de3bd2d233863deadd8d3007dd5220dfc35832
rikutan <163800982+rikutan@users.noreply.github.com>
2024-10-14 16:38:45 +0900

    add プロフィール画像の追加

--- app/src/main/java/com/example/udemypractice1/MainActivity.kt
+++ app/src/main/java/com/example/udemypractice1/MainActivity.kt
@@ -3,20 +3,25 @@ package com.example.udemypractice1
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,15 +36,27 @@ class MainActivity : ComponentActivity() {
             UdemyPractice1Theme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color(0xff7fffff)
                 ) {
                     Column(
-                        horizontalAlignment = Alignment.CenterHorizontally
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.padding(20.dp)
                     ) {
+                        // プロフィール画像
+                        Image(
+                            painter = painterResource(R.drawable.start_ruri),
+                            contentDescription = "プロフィール",
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .size(100.dp)
+                                .clip(RoundedCornerShape(10.dp))
+                        )
+                        Spacer(modifier = Modifier.height(20.dp))
+
                         // 名前
                         Text(
                             text = "有我陸",
-                            color = Color.Gray,
+                            color = Color.Black,
                             fontSize = 16.sp,
                             fontWeight = FontWeight.ExtraBold,
                         )
@@ -47,8 +64,8 @@ class MainActivity : ComponentActivity() {
 
                         // 職業
                         Text(
-                            text = "職業: Androidエンジニア",
-                            color = Color.Gray,
+                            text = "職業: 将来成功する学生(自称)",
+                            color = Color.Black,
                             fontSize = 16.sp
                         )
                     }
@@ -64,15 +81,27 @@ fun MainPreview() {
     UdemyPractice1Theme {
         Surface(
             modifier = Modifier.fillMaxSize(),
-            color = MaterialTheme.colorScheme.background
+            color = Color(0xff7fffff)
         ) {
             Column(
-                horizontalAlignment = Alignment.CenterHorizontally
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.padding(20.dp)
             ) {
+                // プロフィール画像
+                Image(
+                    painter = painterResource(R.drawable.start_ruri),
+                    contentDescription = "プロフィール",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .size(100.dp)
+                        .clip(RoundedCornerShape(10.dp))
+                )
+                Spacer(modifier = Modifier.height(20.dp))
+
                 // 名前
                 Text(
                     text = "有我陸",
-                    color = Color.Gray,
+                    color = Color.Black,
                     fontSize = 16.sp,
                     fontWeight = FontWeight.ExtraBold,
                 )
@@ -80,8 +109,8 @@ fun MainPreview() {
 
                 // 職業
                 Text(
-                    text = "職業: Androidエンジニア",
-                    color = Color.Gray,
+                    text = "職業: 将来成功する学生(自称)",
+                    color = Color.Black,
                     fontSize = 16.sp
                 )
             }
